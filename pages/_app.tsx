ddd1db142395ed93a01bfec1061ae4febb87aebe
Kolya Venturi <me@kolya.co>
2021-08-08 16:56:19 -0700

    Next11 and update the app wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,71 +1,51 @@
 import * as React from 'react';
-import { Disclosure } from '@headlessui/react';
+import {Disclosure} from '@headlessui/react';
 import {AppProps} from 'next/dist/next-server/lib/router/router';
 import 'tailwindcss/tailwind.css';
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-const navigation = [
-  {name: 'Risk', href: '/', current: true}
-];
 
 const App = ({Component, pageProps}: AppProps): JSX.Element => (
   <div className="min-h-screen bg-white">
     <Disclosure as="nav" className="bg-white border-b border-gray-200">
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="flex justify-between h-16">
-              <div className="flex">
-                <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Covid19"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                    alt="Covid19"
-                  />
-                </div>
-                <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className={classNames(
-                        item.current
-                          ? 'border-indigo-500 text-gray-900'
-                          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                        'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-             
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex">
+            <div className="flex-shrink-0 flex items-center select-none">
+              <h1 className="text-3xl font-bold">
+                C<span className="text-blue-500">19</span>
+              </h1>
             </div>
           </div>
+        </div>
+      </div>
     </Disclosure>
 
     <div className="py-10">
       <header>
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold leading-tight text-gray-900">Risk Over Time</h1>
+          <h1 className="text-3xl font-bold leading-tight text-gray-900">
+            U.S. COVID Data Tracker
+          </h1>
+          <p className="text-gray-700">
+            A Covid-19 data tracker, extending the data available on{' '}
+            <a
+              href="https://covidactnow.org"
+              target="_blank"
+              className="underline"
+              rel="noreferrer"
+            >
+              CovidActNow.org
+            </a>
+            .
+          </p>
         </div>
       </header>
       <main>
         <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <Component {...pageProps}/>
+          <Component {...pageProps} />
         </div>
       </main>
     </div>
   </div>
 );
-
 
 export default App;

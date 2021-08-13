6ac2de110212d9b5de242e41e3df768efc835035
Kolya Venturi <me@kolya.co>
2021-08-13 07:26:39 -0700

    API Endpoint

--- constants/urls.ts
+++ constants/urls.ts
@@ -4,7 +4,7 @@ export const GEO_URL_COUNTIES =
   'https://cdn.jsdelivr.net/npm/us-atlas@3/counties-10m.json';
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://c19.api.kolyaventuri.dev/v1'
+    ? 'https://c19.api.blagl.xyz/v1'
     : 'http://localhost:3000/v1';
 
 export const STATE_RISK_TIMESERIES = `${BASE_URL}/states/risk/timeseries`;
